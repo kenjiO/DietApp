f4402422eee4dce693244584a99dc281bc0ad7df
Robert Carswell <rcarswel@westga.edu>
2016-06-15 17:01:42 -0400

    Initial Robert Work

--- DietApp/Model/User.cs
+++ DietApp/Model/User.cs
@@ -6,35 +6,29 @@ using System.Threading.Tasks;
 
 namespace DietApp.Model
 {
+    /// <summary>
+    /// User class, relates to Users table in HealthTrends
+    /// </summary>
     public class User
     {
         private string firstName;
         private string lastName;
         private string email;
 
-        public string FirstName
+        public User()
         {
-            get
-            {
-                return this.firstName;
-            }
-            set
-            {
-                this.firstName = value;
-            }
         }
-
-        public string LastName
-        {
-            get
-            {
-                return this.lastName;
-            }
-            set
-            {
-                this.lastName = value;
-            }
-        }
+        
+        //*********** Getters and Setters **********//
+        public string username { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string password { get; set; }
+        public double initialWeight { get; set; }
+        public double heightFeet { get; set; }
+        public double heightInches { get; set; }
+        public double dailyCalorieGoal { get; set; }
+        public double goalWeight { get; set; }
 
         public string Email
         {
