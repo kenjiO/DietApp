302cd023fd6964446b07e0f1763c24cdab388aee
Kenji Okamoto <kenjio@users.noreply.github.com>
2016-06-16 12:34:41 -0700

    Add code to MainForm to launch the ProfileInfo form

--- DietApp/View/MainForm.cs
+++ DietApp/View/MainForm.cs
@@ -7,19 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DietApp.Controller;
 
 namespace DietApp
 {
     public partial class MainForm : Form
     {
+        private AppController _controller;
+
         public MainForm()
         {
             InitializeComponent();
+            //TODO get a real controller
+            this._controller = new AppController();
         }
 
         private void myHealthTrendsToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void myProfileToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ProfileInfo profileForm = new ProfileInfo(this._controller);
+            profileForm.ShowDialog();
         }
     }
 }
