55e4be4c4236cdabc3ce0eef1b853e235612dd0e
Kenji Okamoto <kenjio@users.noreply.github.com>
2016-06-16 12:57:53 -0700

    Create the form for entering a food item

---
+++ DietApp/View/FoodEntryForm.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace DietApp.View
+{
+    partial class FoodEntryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.foodBox = new System.Windows.Forms.TextBox();
+            this.caloriesBox = new System.Windows.Forms.TextBox();
+            this.fatBox = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.proteinBox = new System.Windows.Forms.TextBox();
+            this.carbohydratesBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // foodBox
+            // 
+            this.foodBox.Location = new System.Drawing.Point(184, 47);
+            this.foodBox.Name = "foodBox";
+            this.foodBox.Size = new System.Drawing.Size(158, 20);
+            this.foodBox.TabIndex = 51;
+            // 
+            // caloriesBox
+            // 
+            this.caloriesBox.Location = new System.Drawing.Point(184, 95);
+            this.caloriesBox.Name = "caloriesBox";
+            this.caloriesBox.Size = new System.Drawing.Size(158, 20);
+            this.caloriesBox.TabIndex = 52;
+            // 
+            // fatBox
+            // 
+            this.fatBox.Location = new System.Drawing.Point(184, 139);
+            this.fatBox.Name = "fatBox";
+            this.fatBox.Size = new System.Drawing.Size(158, 20);
+            this.fatBox.TabIndex = 53;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLabel.Location = new System.Drawing.Point(118, 45);
+            this.usernameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(46, 20);
+            this.usernameLabel.TabIndex = 54;
+            this.usernameLabel.Text = "Food";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(98, 93);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 55;
+            this.label1.Text = "Calories";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(108, 137);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 56;
+            this.label2.Text = "Fat (g)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(82, 185);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 57;
+            this.label3.Text = "Protein (g)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(28, 231);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 20);
+            this.label4.TabIndex = 58;
+            this.label4.Text = "Carbohydrates (g)";
+            // 
+            // proteinBox
+            // 
+            this.proteinBox.Location = new System.Drawing.Point(184, 187);
+            this.proteinBox.Name = "proteinBox";
+            this.proteinBox.Size = new System.Drawing.Size(158, 20);
+            this.proteinBox.TabIndex = 59;
+            // 
+            // carbohydratesBox
+            // 
+            this.carbohydratesBox.Location = new System.Drawing.Point(184, 233);
+            this.carbohydratesBox.Name = "carbohydratesBox";
+            this.carbohydratesBox.Size = new System.Drawing.Size(158, 20);
+            this.carbohydratesBox.TabIndex = 60;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(171, 279);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 61;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(267, 279);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 62;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.searchButton);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(377, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(201, 208);
+            this.panel1.TabIndex = 63;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(-1, 13);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(201, 20);
+            this.label5.TabIndex = 55;
+            this.label5.Text = "Food Lookup";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 64;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(61, 88);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 64;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // FoodEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 337);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.carbohydratesBox);
+            this.Controls.Add(this.proteinBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.fatBox);
+            this.Controls.Add(this.caloriesBox);
+            this.Controls.Add(this.foodBox);
+            this.Name = "FoodEntry";
+            this.Text = "Food Entry";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox foodBox;
+        private System.Windows.Forms.TextBox caloriesBox;
+        private System.Windows.Forms.TextBox fatBox;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox proteinBox;
+        private System.Windows.Forms.TextBox carbohydratesBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ DietApp/View/FoodEntryForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DietApp.View
+{
+    public partial class FoodEntryForm : Form
+    {
+        public FoodEntryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
