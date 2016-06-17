67b244ba3edeec836099b71e1e1d53c1119eb58d
Kenji Okamoto <kenjio@users.noreply.github.com>
2016-06-17 15:44:07 -0700

    Update test data script to delete users first and to specify the userIds

--- Resources/Load Test Data.sql
+++ Resources/Load Test Data.sql
@@ -1,12 +1,18 @@
 
+DELETE FROM users
+
+SET IDENTITY_INSERT users ON 
+
 -- password abc
-INSERT INTO users (username, firstName, lastName, email, password, initialWeight, heightInches, dailyCalorieGoal, goalWeight)
-VALUES ('aa', 'Al', 'Adams', 'aladams@example.com', '0x40bd001563085fc35165329ea1ff5c5ecbdbbeef', 220, 70, 2500, 180) 
+INSERT INTO users (userId, username, firstName, lastName, email, password, initialWeight, heightInches, dailyCalorieGoal, goalWeight)
+VALUES (1,'aa', 'Al', 'Adams', 'aladams@example.com', '0x40bd001563085fc35165329ea1ff5c5ecbdbbeef', 220, 70, 2500, 180) 
 
 -- password 123
-INSERT INTO users (username, firstName, lastName, email, password, initialWeight, heightInches, dailyCalorieGoal, goalWeight)
-VALUES ('bb', 'Betsy', 'Brown', 'bbrown@example.com', '0xa9993e364706816aba3e25717850c26c9cd0d89d', 190, 65, 2000, 150) 
+INSERT INTO users (userId, username, firstName, lastName, email, password, initialWeight, heightInches, dailyCalorieGoal, goalWeight)
+VALUES (2, 'bb', 'Betsy', 'Brown', 'bbrown@example.com', '0xa9993e364706816aba3e25717850c26c9cd0d89d', 190, 65, 2000, 150) 
 
 -- password xyz
-INSERT INTO users (username, password)
-VALUES ('cc', '0x66b27417d37e024c46526c2f6d358a754fc552f3') 
+INSERT INTO users (userId, username, password)
+VALUES (3, 'cc', '0x66b27417d37e024c46526c2f6d358a754fc552f3') 
+
+SET IDENTITY_INSERT users OFF 
