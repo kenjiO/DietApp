702890e246e8e3312e5f3c3d51c907bc00926b5c
rcarswel <rcarswel@westga.edu>
2016-06-19 07:29:29 -0400

    Add start DAL.UserDAL testing

---
+++ DietAppTests/DAL/UserDALTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DietApp.Model;
+using DietApp.DAL;
+using System.Collections.Generic;
+
+namespace DietAppTests.DAL
+{
+    [TestClass]
+    public class UserDALTest
+    {
+        /// <summary>
+        /// Test for return of user data
+        /// </summary>
+        [TestMethod]
+        public void TestGetUserAA()
+        {
+            //Set User AA
+            String username = "aa";
+
+            //Get User AA
+            Users aa = UsersDAL.getUserData("aa");
+
+            //Check Results, expect AA
+            Assert.AreEqual(username, aa.username, "User's username not " + username + ".");
+
+        }
+    }
+}
