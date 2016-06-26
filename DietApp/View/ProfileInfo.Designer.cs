9c2e917fd6964850bc1f92fcd6283c401a0cfa20
Kenji Okamoto <kenjio@users.noreply.github.com>
2016-06-26 09:41:54 -0700

    On the Profile Info form disable the username text box since it can't be changed

--- DietApp/View/ProfileInfo.Designer.cs
+++ DietApp/View/ProfileInfo.Designer.cs
@@ -199,6 +199,7 @@
             // 
             // usernameBox
             // 
+            this.usernameBox.Enabled = false;
             this.usernameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.usernameBox.Location = new System.Drawing.Point(227, 156);
             this.usernameBox.Margin = new System.Windows.Forms.Padding(2);
