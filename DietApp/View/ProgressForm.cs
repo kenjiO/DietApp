9285133f75bb5876367dff98d1d8accb994e2583
Kaleigh <kkendri4@my.westga.edu>
2016-07-14 22:13:51 -0400

    Implemented user story "As a user, I can set a goal weight and track my progress towards that goal."  This involved changes to the way BMI is calculated (created a variable ideal to take the place of goalWeight in the calculations).

--- DietApp/View/ProgressForm.cs
+++ DietApp/View/ProgressForm.cs
@@ -55,35 +55,35 @@ namespace DietApp.View
 
             if (BMI <= 15)
             {
-                msg = "Very severely underweight\n" + this.weightDifference();
+                msg = "Very severely underweight\n" + this.weightDifference() + "\n" + this.goalWeight();
             }
             else if (BMI > 15 && BMI <= 16)
             {
-                msg = "Severely underweight\n" + this.weightDifference();
+                msg = "Severely underweight\n" + this.weightDifference() + "\n" + this.goalWeight();
             }
             else if (BMI > 16 && BMI <= 18.5)
             {
-                msg = "Underweight\n" + this.weightDifference();
+                msg = "Underweight\n" + this.weightDifference() + "\n" + this.goalWeight();
             }
             else if (BMI > 18.5 && BMI <= 25)
             {
-                msg = "Normal (healthy weight)\n Congratulations! Your BMI is normal, which means you are a healthy weight for your height.";
+                msg = "Normal (healthy weight)\n Congratulations! Your BMI is normal, which means you are a healthy weight for your height." + "\n" + this.goalWeight();
             }
             else if (BMI > 25 && BMI <= 30)
             {
-                msg = "Overweight\n" + this.weightDifference();
+                msg = "Overweight\n" + this.weightDifference() + "\n" + this.goalWeight();
             }
             else if (BMI > 30 && BMI <= 35)
             {
-                msg = "Obese Class I (Moderately obese)\n" + this.weightDifference();
+                msg = "Obese Class I (Moderately obese)\n" + this.weightDifference() + "\n" + this.goalWeight();
             }
             else if (BMI > 35 && BMI <= 40)
             {
-                msg = "Obese Class II (Severely obese)\n" + this.weightDifference();
+                msg = "Obese Class II (Severely obese)\n" + this.weightDifference() + "\n" + this.goalWeight();
             }
             else if (BMI > 40)
             {
-                msg = "Obese Class III (Very severely obese)\n" + this.weightDifference();
+                msg = "Obese Class III (Very severely obese)\n" + this.weightDifference() + "\n" +this.goalWeight();
             }
             else
             {
@@ -100,23 +100,52 @@ namespace DietApp.View
         private String weightDifference()
         {
             var BMI = this.theProgress.newBMI;
+            int ideal;
             var msg = "";
             if (BMI <= 18.5)
             {
-                this.theUser.goalWeight = Convert.ToInt32(Math.Round((18.6 / 703) * Math.Pow(this.theUser.heightInches, 2)));
-                msg = "You need to gain " + (this.theUser.goalWeight - this.theProgress.newWeight) + " lb(s). to reach a healthy BMI.\n" +
-                    "This means you should aim for a weight of approximately " + this.theUser.goalWeight + " lbs.";
+                ideal = Convert.ToInt32(Math.Round((18.6 / 703) * Math.Pow(this.theUser.heightInches, 2)));
+                msg = "You need to gain " + (ideal - this.theProgress.newWeight) + " lb(s). to reach a healthy BMI.\n" +
+                    "This means you should aim for a weight of approximately " + ideal + " lbs.";
             }
             else if (BMI > 25)
             {
-                this.theUser.goalWeight = Convert.ToInt32(Math.Round((25.0 / 703) * Math.Pow(this.theUser.heightInches, 2)));
-                msg = "You need to lose " + (this.theProgress.newWeight - this.theUser.goalWeight) + " lb(s). to reach a healthy BMI.\n" +
-                    "This means you should aim for a weight of approximately " + this.theUser.goalWeight + " lbs.";
+                ideal = Convert.ToInt32(Math.Round((25.0 / 703) * Math.Pow(this.theUser.heightInches, 2)));
+                msg = "You need to lose " + (ideal - this.theUser.goalWeight) + " lb(s). to reach a healthy BMI.\n" +
+                    "This means you should aim for a weight of approximately " + ideal + " lbs.";
             }
             else
             {
                 msg = "error";
             }
+
+            return msg;
+        }
+
+        /// <summary>
+        /// Determines how much weight needs to be lost or gained to reach the user's goal weight and returns it as a string.
+        /// </summary>
+        /// <returns></returns>
+        private String goalWeight()
+        {
+            var msg = "";
+            if (this.theUser.goalWeight < this.theProgress.newWeight)
+            {
+                msg = "\n Goal Weight: \n You need to lose "+ (this.theProgress.newWeight - this.theUser.goalWeight) +" lb(s) to meet your goal weight.";
+            }
+            else if (this.theUser.goalWeight > this.theProgress.newWeight)
+            {
+                msg = "\n Goal Weight: \n You need to gain " + (this.theUser.goalWeight - this.theProgress.newWeight) + " lb(s) to meet your goal weight.";
+            }
+            else if (this.theUser.goalWeight == this.theProgress.newWeight)
+            {
+                msg = "\n Goal Weight: \n Congratulations!  You've met your goal weight.";
+            }
+            else
+            {
+                msg = "error";
+            }
+
 
             return msg;
         }
