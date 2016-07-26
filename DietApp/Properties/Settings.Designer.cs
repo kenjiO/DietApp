af9b425989ed39231e5f00b304cd2e91f7f3be32
Kenji Okamoto <kenjio@users.noreply.github.com>
2016-07-26 16:18:55 -0700

    Use hosted DB instead of local DB

--- DietApp/Properties/Settings.Designer.cs
+++ DietApp/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DietApp.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,8 @@ namespace DietApp.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost;Initial Catalog=DietApp;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=dietapp.cild60usm1fr.us-east-1.rds.amazonaws.com;Initial Catalog=Diet" +
+            "App;User ID=uwgproject;Password=r97kB###493")]
         public string DietAppConnectionString {
             get {
                 return ((string)(this["DietAppConnectionString"]));
